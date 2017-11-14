6091aae7b6f5f11f0e370e26b5ee11cc87d67d6c
axetroy <troy450409405@gmail.com>
2017-11-14 22:39:19 +0800

    feat: 监听signal，并退出进程

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"github.com/axetroy/go-upload/config"
 	"github.com/axetroy/go-upload/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"fmt"
 )
 
 func main() {
@@ -10,6 +14,18 @@ func main() {
 	var (
 		err error
 	)
+
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+	go func() {
+		s := <-sigc
+		fmt.Print("signal: ", s)
+		os.Exit(1)
+	}()
 
 	// init anything
 	if err = config.Init(); err != nil {
