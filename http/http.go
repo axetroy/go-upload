1243c5764ef794a01f2ed1455ba8902fd465e196
axetroy <troy450409405@gmail.com>
2017-11-22 10:32:05 +0800

    feat: support cors

--- http/http.go
+++ http/http.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/axetroy/go-upload/config"
 	"github.com/axetroy/gin-uploader"
+	"github.com/itsjamie/gin-cors"
+	"time"
 )
 
 var (
@@ -15,6 +17,16 @@ func RunServer() (err error) {
 	Router = gin.Default()
 	Router.Use(gin.Logger())
 	Router.Use(gin.Recovery())
+
+	Router.Use(cors.Middleware(cors.Config{
+		Origins:         "*",
+		Methods:         "GET, PUT, POST, DELETE",
+		RequestHeaders:  "Origin, Authorization, Content-Type",
+		ExposedHeaders:  "",
+		MaxAge:          50 * time.Second,
+		Credentials:     true,
+		ValidateHeaders: false,
+	}))
 
 	if upload, download, err := uploader.New(Router, config.Config.Upload); err != nil {
 		return err
