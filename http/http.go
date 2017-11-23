f8d06a764abfcd6dddb90ac766d10bef489712ff
axetroy <troy450409405@gmail.com>
2017-11-23 15:30:58 +0800

    fix: cors

--- http/http.go
+++ http/http.go
@@ -21,10 +21,10 @@ func RunServer() (err error) {
 	Router.Use(cors.Middleware(cors.Config{
 		Origins:         "*",
 		Methods:         "GET, PUT, POST, DELETE",
-		RequestHeaders:  "Origin, Authorization, Content-Type",
+		RequestHeaders:  "Origin, X-Requested-With, X_Requested_With, Content-Type, Accept, Authentication, Authorization, X-Server, X-Password-Pay",
 		ExposedHeaders:  "",
-		MaxAge:          50 * time.Second,
-		Credentials:     true,
+		MaxAge:          60 * time.Second,
+		Credentials:     true,		// cookies
 		ValidateHeaders: false,
 	}))
 
